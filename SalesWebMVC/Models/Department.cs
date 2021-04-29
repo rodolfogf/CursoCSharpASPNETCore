89cbee34c6a49371c4174f76de9ca0761583bb94
rodolfogf <rodolfogf@gmail.com>
2021-04-28 23:06:23 -0300

    251. Outras entidades e segunda migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMVC.Models
+
+
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime inicial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(inicial, final));
+        }
     }
 }
