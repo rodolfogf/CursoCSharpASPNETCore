637e1f2a09466e9ee8a264ff830f2ac77c92c7b9
Rodolfo Faria <rodolfogf@gmail.com>
2021-05-12 23:19:44 -0300

    255. Tela simples de cadastro

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
@@ -18,6 +19,8 @@ namespace SalesWebMVC.Controllers
             _sellerService = sellerService;
         }
         
+        //OBS: IActionResult é o tipo de retorno de todas as ações
+
         //Esse método Index irá chamar a operação FindAll que está no SellerService
         public IActionResult Index()
         {
@@ -29,7 +32,22 @@ namespace SalesWebMVC.Controllers
             return View(list);
 
             //Em seguida à essa implementação será implementada na página Index um código de template para mostrar os vendedores na tela
+        }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        /*Essa operação vai receber um objeto vendedor que veio na requisição. 
+        Basta inseri-lo como parâmetro, o framework irá entender e o fará automaticamente.*/
+        
+        [HttpPost] //Indica que essa operação será de Post, e não de Get
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller); //Uma vez inserido o seller no banco de dados, a requisição será redirecionada para a ação Index, implementação abaixo
+            return RedirectToAction(nameof(Index));
         }
     }
 }
